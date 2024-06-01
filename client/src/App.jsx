01f58cc8907e4fafb6a8b15bafc4db78a24ad186
AbubekerJ <abubeker.jemal03@gmail.com>
2024-06-01 16:55:45 +0100

    create pages and implement react router dom for routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import SignIn from './pages/SignIn.jsx'
+import SignUp from './pages/SignUp.jsx'
+import About from './pages/About.jsx'
+import Profile from './pages/Profile.jsx'
 
 function App() {
-  return (
-    <div className=' ' >
-      <h1 className='text-6xl font-bold underline text-center text-red-500'>hello</h1>
-    </div>
-  )
+  return <BrowserRouter>
+  <Routes>
+
+    <Route path='/' element = {<Home/>}/>
+    <Route path='/SignIn' element = {<SignIn/>}/>
+    <Route path='/SignUp' element = {<SignUp/>}/>
+    <Route path='/About' element = {<About/>}/>
+    <Route path='/Profile' element = {<Profile/>}/>
+   
+    
+  </Routes>
+  
+  </BrowserRouter>
 }
 
 export default App
