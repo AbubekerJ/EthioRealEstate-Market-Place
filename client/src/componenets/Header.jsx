80d35f0febb803776ed72ea9765d452975460be4
AbubekerJ <abubeker.jemal03@gmail.com>
2024-06-04 20:14:50 +0100

    create sign up page Ui

--- client/src/componenets/Header.jsx
+++ client/src/componenets/Header.jsx
@@ -3,37 +3,45 @@ import { Link } from 'react-router-dom'
 
 function Header() {
   return (
-    <header className='bg-orange-200  ' >
-        <div className='flex items-center justify-between mx-auto max-w-6xl p-2'>
-        <Link to='/'>  <h1 className='flex flex-wrap font-bold text-sm sm:text-xl p-5 '>
-      <span className='text-amber-700 text-bold '>Ethio</span>
-      <span className='text-slate-800'>RealState</span>
-    </h1></Link>
-      
-     <form className='bg-amber-100 flex p-2 shadow-md rounded-xl items-center'  >
-        <input className='  bg-transparent focus:outline-none w-24  sm:w-52' type="text" placeholder='Search...' />
-        <FaSearch/>
-     </form>
-     <ul className='flex gap-7 text-slate-900 '>
-           <Link to='/'>
-           <li className='hidden sm:inline hover:underline'>
-          Home 
-        </li></Link>
-        
-         <Link to='About'> <li className='hidden sm:inline hover:underline'>
-          About
-        </li></Link>
-       
-           <Link to = '/SignIn'>
-        <li className='hidden sm:inline hover:underline'>
-          SignIn
-        </li>
+    <header className='bg-gray-800 shadow-md'>
+    <div className='flex items-center justify-between mx-auto max-w-6xl p-4'>
+      <Link to='/'>
+        <h1 className='flex flex-wrap font-bold text-lg sm:text-2xl p-5'>
+          <span className='text-amber-500 text-bold'>Ethio</span>
+          <span className='text-white'>RealState</span>
+        </h1>
+      </Link>
+
+      <form className='bg-gray-700 flex p-2 shadow-inner rounded-lg items-center'>
+        <input 
+          className='bg-transparent focus:outline-none w-24 sm:w-52 text-white placeholder-gray-400' 
+          type="text" 
+          placeholder='Search...' 
+        />
+        <FaSearch className='text-white ml-2'/>
+      </form>
+
+      <ul className='flex gap-7 text-white'>
+        <Link to='/'>
+          <li className='hidden sm:inline hover:underline transition duration-200'>
+            Home
+          </li>
         </Link>
 
-     </ul>
-        </div>
-     
-    </header>
+        <Link to='/About'>
+          <li className='hidden sm:inline hover:underline transition duration-200'>
+            About
+          </li>
+        </Link>
+
+        <Link to='/SignIn'>
+          <li className='hidden sm:inline hover:underline transition duration-200'>
+            Sign In
+          </li>
+        </Link>
+      </ul>
+    </div>
+  </header>
   )
 }
 
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,43 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-function SignUp() {
+const SignUp = () => {
   return (
-    <div>
-      sign up
+    <div className='max-w-md mx-auto bg-white shadow-lg rounded-lg p-8 mt-12'>
+      <h1 className='font-semibold text-center text-4xl text-gray-800 mb-8'>Sign Up</h1>
+      
+      <form className='flex flex-col gap-6'>
+        <input 
+          type="text" 
+          placeholder='Username' 
+          className='border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800' 
+          id='userName' 
+        />
+        <input 
+          type="email" 
+          placeholder='Email' 
+          className='border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800' 
+          id='email' 
+        />
+        <input 
+          type="password" 
+          placeholder='Password' 
+          className='border border-gray-300 p-3 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-800' 
+          id='password' 
+        />
+        <button 
+          className='border p-3 rounded-lg bg-gray-800 text-white uppercase hover:bg-gray-600 transition duration-300 ease-in-out'>
+          Sign Up
+        </button>
+      </form>
+      <div className='flex mt-3 gap-1'>
+        <p>Have an Account?</p>
+        <Link to='/signIn'>
+            <span className='text-blue-800'>Sign In</span>
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
