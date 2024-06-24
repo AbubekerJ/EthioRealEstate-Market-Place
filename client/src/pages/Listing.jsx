02cc629ee8970a3763469dc6e10fcb3b88e0a514
AbubekerJ <abubeker.jemal03@gmail.com>
2024-06-24 10:09:18 +0100

    add slider to the listing page

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -1,25 +1,37 @@
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore from 'swiper';
+import { useSelector } from 'react-redux';
+import { Navigation } from 'swiper/modules';
+import 'swiper/css/bundle';
+
+
+SwiperCore.use(Navigation)
 
 function Listing() {
   const params = useParams()
   const [listing, setListing] = useState(null)
+  const [error , setError]=useState(false)
   console.log(listing)
 
   useEffect(() => {
     const fetchData = async () => {
+      setError(false)
       try {
         const res = await fetch(`http://localhost:3000/api/listing/myListing/${params.listingId}`)
         const data = await res.json()
         console.log(data)
         if (data.success ==='false'){
-          console.log(data.message)
+         
+          setError(data.message)
           return;
 
         }
         setListing(data)
       } catch (error) {
-        console.log(error)
+        
+        setError(error)
       }
     }
     fetchData()
@@ -27,8 +39,24 @@ function Listing() {
 
   return (
     <div>
-      {listing && 
-       <img src={listing.imageUrls[0]} alt="" /> }
+
+      {error&& error}
+
+      {listing && !error&& 
+       <Swiper navigation>
+        {listing.imageUrls.map((url)=>(
+            <SwiperSlide key={listing._id}>
+               <div
+                  className='h-[550px]'
+                  style={{
+                    background: `url(${url}) center no-repeat`,
+                    backgroundSize: 'cover',
+                  }}
+                ></div>
+            </SwiperSlide>
+        ))}
+        
+        </Swiper>}
     </div>
   )
 }
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -31,7 +31,7 @@ const SignUp = () => {
           console.log(data)
           setLoading(false)
           setError(null)
-          alert(data)
+          
           navigate('/signIn')
       } catch (error) {
         setError(error.message)
